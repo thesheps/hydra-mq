c914d9fc0aa3b41135726d6db53bba3ebcc84251
Chris Shepherd <thesheps@gmail.com>
2017-08-18 20:43:41 +0100

    Add project files.

---
+++ HydraMQ/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HydraMQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
